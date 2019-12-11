d4366cade96eeedcc614d60aac452d631126e590
x-Nitish Poojari <x-nitish.poojari@coyote.com>
2019-12-11 15:04:38 +0530

    initial commit

---
+++ DataTypes/datatypes.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//declare and assign
+	var final = 3
+
+	//declare and initialize variable
+	var intTotal int
+	intTotal = 3
+
+	//multiple variable assign
+	var val1, val2 = 3, "string"
+
+	//another way to assign value
+	directAssign := "Hello World"
+
+	fmt.Println(final, intTotal, val1, val2, directAssign)
+
+}
